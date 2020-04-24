82649172b1d4a8632591cf8883b31bdf7ca12a1e
JihyeHwang09 <raye9053@gmail.com>
2020-04-24 17:49:52 +0900

    요구사항6- 로그인 상태 유무에 따라 사용자 목록 페이지를 출력한다

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -16,6 +16,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,13 +42,16 @@ public class RequestHandler extends Thread {
 
             String url = HttpRequestUtils.getUrl(line);
             Map<String, String> headers = new HashMap<>();
-
+            boolean logined = false;
             while(!line.equals("")) {
                 log.debug("header: {}", line);
                 line = br.readLine();
                 String[] headerTokens = line.split(": ");
                 if (headerTokens.length == 2) {
                     headers.put(headerTokens[0], headerTokens[1]);
+                }
+                if (line.contains("Cookie")) {
+                    logined = isLogin(line);
                 }
             }
             log.debug("Content-Length: {}", headers.get("Content-Length"));
@@ -59,6 +63,8 @@ public class RequestHandler extends Thread {
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User: {}", user);
                 DataBase.addUser(user);
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos);
             } else if(url.equals("/user/login")) {
                 String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
                 log.debug("Request Body: {}", requestBody);
@@ -68,23 +74,52 @@ public class RequestHandler extends Thread {
                 if (user == null) {
                     responseResource(out, "/user/login_failed.html");
                     return;
-                } else if (user.getPassword().equals(params.get("password"))) {
+                }
+
+                if (user.getPassword().equals(params.get("password"))) {
                     DataOutputStream dos = new DataOutputStream(out);
                     response302LoginSuccessHeader(dos);
                 } else {
-                    log.debug("Password Mismatch!!");
+                    responseResource(out, "/user/login_failed.html");
                 }
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos);
-            } else {
+            } else if("/user/list".equals(url)) {
+                if (!logined) {
+                    responseResource(out, "/user/login.html");
+                    return;
+                }
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
                 DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
                 response200Header(dos, body.length);
                 responseBody(dos, body);
+            } else {
+                responseResource(out, url);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void responseResource(OutputStream out, String url) throws IOException {
