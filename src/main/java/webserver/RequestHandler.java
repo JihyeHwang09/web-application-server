55878edd9fba52645fba952146454d50650275c7
JihyeHwang09 <raye9053@gmail.com>
2020-04-24 10:50:58 +0900

    사용자가 http://localhost:8080/index.html로 접속했을 때, index.html을 읽어 클라이언트에 응답한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -27,18 +29,24 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
-//            System.out.println("request: " + line);
-            log.debug("requset line: {}", line);
+            if (line == null) {
+                return;
+            }
+
+            String[] splited = line.split(" ");
+            // 요청 헤더에서 /index.html 부분만 꺼내온다.
+            String path = splited[1];
+            log.debug("request path: {}", path);
+
 //            line != null로 하면 무한루프에 빠질 수 있다.
 //            http 요청의 마지막 줄은 빈문자열로 되어 있다는 점을 이용한다.
-            while(!line.equals("")) {
-                line = br.readLine();
-//                System.out.println("request: " + line);
-                log.debug("header: {}", line);
-                // log로 출력할 경우, 어느 thread에서 실행되는지, 어느 클래스에서 출력되는지 알 수 있다.
-            }
+//            while(!line.equals("")) {
+//                log.debug("header: {}", line);
+//                line = br.readLine();
+//                // log로 출력할 경우, 어느 thread에서 실행되는지, 어느 클래스에서 출력되는지 알 수 있다.
+//            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
