6d9c8be00464809d58f54e747269c4a29bb16e27
JihyeHwang09 <raye9053@gmail.com>
2020-04-24 13:38:13 +0900

    요구사항2- GET 방식으로 회원가입 구현하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -13,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,6 +35,17 @@ public class RequestHandler extends Thread {
             if (line == null) {
                 return;
             }
+
+            String url = HttpRequestUtils.getUrl(line);
+            if (url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+//                String requestPath = url.substring(0, index);
+                String queryString = url.substring(index + 1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User: {}", user);
+                url = "/index.html";
+            }
 //            line != null로 하면 무한루프에 빠질 수 있다.
 //            http 요청의 마지막 줄은 빈문자열로 되어 있다는 점을 이용한다.
 //            while(!line.equals("")) {
@@ -40,8 +53,6 @@ public class RequestHandler extends Thread {
 //                line = br.readLine();
 //                // log로 출력할 경우, 어느 thread에서 실행되는지, 어느 클래스에서 출력되는지 알 수 있다.
 //            }
-
-            String url = HttpRequestUtils.getUrl(line);
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
