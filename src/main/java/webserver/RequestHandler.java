70ffa3106d0f8afbdeeabc2e541101de098132ef
JihyeHwang09 <raye9053@gmail.com>
2020-04-24 10:12:18 +0900

    사용자의 요청을 읽은 후 로그로 출력한다.
    - BufferedReader를 이용해 사용자의 요청을 읽는다.
    - Logger를 이용해 실행되는 Thread와 Class를 확인할 수 있다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,15 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,7 +25,18 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+//            System.out.println("request: " + line);
+            log.debug("requset line: {}", line);
+//            line != null로 하면 무한루프에 빠질 수 있다.
+//            http 요청의 마지막 줄은 빈문자열로 되어 있다는 점을 이용한다.
+            while(!line.equals("")) {
+                line = br.readLine();
+//                System.out.println("request: " + line);
+                log.debug("header: {}", line);
+                // log로 출력할 경우, 어느 thread에서 실행되는지, 어느 클래스에서 출력되는지 알 수 있다.
+            }
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
